239a2a60f45040fad0a656c94d821ad05b7865a9
Shruti <shruti@simformsolutions.com>
2023-04-05 12:28:13 +0530

    Removed comments

--- src/Data.java
+++ src/Data.java
@@ -1,10 +1,6 @@
-//FINAL CLASS
 final class Data {
-
-    //FINAL INSTANCE VARIABLE
     final String COMPANY = "Simform Solutions";
 
-    //FINAL METHOD
     final void display(int id, String name) {
         System.out.println(
                 "ID : " + id + "\n" +
